00975b0de8e391ad8c12e60143030a384f07a32c
bimapw4 <bimapw4@gmail.com>
2025-06-25 17:17:37 +0700

    [controller] add auth controller

---
+++ controller/auth/auth.js
@@ -0,0 +1,10 @@
+import authService from '../../services/auth/auth.js';
+
+export const login = async (req, res) => {
+  try {
+    const result = await authService.login(req.body);
+    res.json(result);
+  } catch (err) {
+    res.status(401).json({ error: err.message });
+  }
+};
