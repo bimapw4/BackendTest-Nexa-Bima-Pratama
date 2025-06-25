c2aef406d2d881c65af3b40d95a0750e52359db2
bimapw4 <bimapw4@gmail.com>
2025-06-25 17:15:57 +0700

    [bootstrap] add file bootstrap for connect to db

---
+++ bootstrap/db.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+export default connection;
