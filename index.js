8b0d678a335f2c09c9405b5b339bf4002f976b33
bimapw4 <bimapw4@gmail.com>
2025-06-25 17:20:01 +0700

    [index] mount routes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import 'dotenv/config'
+
+import router from './routes/routes.js';
+
+
 const app = express();
-const port = "3000";
+const port = process.env.PORT || "3000";
+
+app.use(express.json());
+
+app.use(router);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(` ${process.env.APP_NAME} listening on port ${port}`);
 });
